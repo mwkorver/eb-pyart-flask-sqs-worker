6c664db97c992e3dbdd5d44896f8f4becb3dcd46
root <root@ip-10-234-3-209.ec2.internal>
2015-12-31 20:01:33 +0000

    boto 2.x additions

--- application.py
+++ application.py
@@ -21,7 +21,8 @@ import boto
 import urllib.request	
 
 local_Dir = '/tmp/'
-url_S3_Prefix = 'http://s3.amazonaws.com/'                      
+url_S3_Prefix = 'http://s3.amazonaws.com/'
+log_bucket = 'usgs-naip'                      
 
 # Create and configure the Flask app
 application = flask.Flask(__name__)
@@ -57,7 +58,30 @@ def customer_registered():
                 os.makedirs(os.path.dirname(file_name))		
 
             open(file_name, 'a+')
+            # this writes to local
             urllib.request.urlretrieve(url, file_name)
+
+            s3_conn = boto.connect_s3()
+            srcBucket = s3_conn.get_bucket(bucketName, validate=False)
+            from boto.s3.key import Key
+            srcKey = Key(srcBucket)
+            srcKey.key = objectKey
+            srcKey.get_contents_to_filename('/tmp/test.test')
+
+
+            # do something to the data here.
+
+            # Boto 2.x
+            s3_conn = boto.connect_s3()
+            logBucket = s3_conn.get_bucket(log_bucket, validate=False)
+            from boto.s3.key import Key
+            logKey = Key(logBucket)
+            logKey.key = objectKey
+            logKey.set_contents_from_filename(file_name)
+
+            # Boto 3
+            # s3 = boto3.resource('s3')
+            # s3.Object(log_bucket, objectKey).put(Body=open(file_name, 'rb'))            
 
             response = Response(value, status=200)
 
