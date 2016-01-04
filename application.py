9926e05eac78447d76506121ef31687723b6acc5
Mark Korver <mwkorver@gmail.com>
2016-01-04 06:15:59 +0000

    only boto for read and write to S3

--- application.py
+++ application.py
@@ -11,19 +11,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# just test line here xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
+import flask
+import boto
 import logging
 import os
 import json
-import flask
 from flask import request, Response
-import boto
-import urllib.request	
 
 local_Dir = '/tmp/'
-url_S3_Prefix = 'http://s3.amazonaws.com/'
-log_bucket = 'usgs-naip'                      
+log_bucket = 'eb-py-flask-sqs-worker-log'
+radarSite = 'KCLEx'                      
 
 # Create and configure the Flask app
 application = flask.Flask(__name__)
@@ -32,7 +30,7 @@ application.debug = application.config['FLASK_DEBUG'] in ['true', 'True']
 
 @application.route('/nexrad', methods=['POST'])
 def customer_registered():
-    """Send an e-mail using SES"""
+    """Process NEXRAD data using by parsing SQS message"""
     response = None
     if request.json is None:
         # Expect application/json request
@@ -42,59 +40,51 @@ def customer_registered():
         try:
             # If the message has an SNS envelope, extract the inner message
             if 'TopicArn' in request.json and 'Message' in request.json:
-            	#if 'TopicArn' in request.json:
-                logging.exception('in the test loop tttttttttttttttttttttttttttttttttttttttttttttttt')
                 message = json.loads(request.json['Message'])                
-                bucketName = message['Records'][0]['s3']['bucket']['name']
-                objectKey = message['Records'][0]['s3']['object']['key']
-                value = bucketName + '/' + objectKey
-                #response = Response(value, status=200)
             else:
-                response = Response("in else part", status=200)    
-                # message = request.json
+                message = json.loads(request.json)
 
-            url = url_S3_Prefix + bucketName + "/" + objectKey
-            file_name = local_Dir + objectKey
-            if not os.path.exists(os.path.dirname(file_name)):
-                os.makedirs(os.path.dirname(file_name))		
+            bucketName = message['Records'][0]['s3']['bucket']['name']
+            objectKey = message['Records'][0]['s3']['object']['key']
+            #value = bucketName + '/' + objectKey
 
-            open(file_name, 'a+')
-            # this writes to local
-            urllib.request.urlretrieve(url, file_name)
+            # Filter for a particular station
+            if radarSite in objectKey:
 
-            s3_conn = boto.connect_s3()
-            srcBucket = s3_conn.get_bucket(bucketName, validate=False)
-            from boto.s3.key import Key
-            srcKey = Key(srcBucket)
-            srcKey.key = objectKey
-            srcKey.get_contents_to_filename('/tmp/test.test')
+                file_name = local_Dir + objectKey
+                if not os.path.exists(os.path.dirname(file_name)):
+                    os.makedirs(os.path.dirname(file_name))		
 
-            # Do something to the data here.
+                s3_conn = boto.connect_s3()
+                srcBucket = s3_conn.get_bucket(bucketName, validate=False)
+                from boto.s3.key import Key
+                srcKey = Key(srcBucket)
+                srcKey.key = objectKey
+                srcKey.get_contents_to_filename(file_name)
 
-            # Boto 2.x
-            s3_conn = boto.connect_s3()
-            logBucket = s3_conn.get_bucket(log_bucket, validate=False)
-            from boto.s3.key import Key
-            logKey = Key(logBucket)
-            logKey.key = objectKey
-            logKey.set_contents_from_filename(file_name)
+                # Do something to the data here.
 
-            # Boto 3
-            # s3 = boto3.resource('s3')
-            # s3.Object(log_bucket, objectKey).put(Body=open(file_name, 'rb'))            
+                # Writes result to own S3 bucket using Boto 2.x
+                s3_conn = boto.connect_s3()
+                logBucket = s3_conn.get_bucket(log_bucket, validate=False)
+                from boto.s3.key import Key
+                logKey = Key(logBucket)
+                logKey.key = objectKey
+                logKey.set_contents_from_filename(file_name)
 
-            response = Response(value, status=200)
+                response = Response("processed NEXRAD data:  " + objectKey, status=200)
+
+            else:
+                response = Response("This data does not satisfy filter:  " + objectKey, status=200)
 
         except Exception as ex:
             logging.exception('Error processing SQS message: %s' % request.json)
-            # response = Response(ex.message, status=500))
-            response = Response("exception part -eeeeeeeeeeeeeeeee", status=500)
+            response = Response("exception", status=500)
             raise
-            # response = Response(flask.jsonify(request.json), status=500)
 
     return response
 
-# https://codeseekah.com/2012/10/28/dubugging-flask-applications-under-uwsgi/
+# from https://codeseekah.com/2012/10/28/dubugging-flask-applications-under-uwsgi/
 if ( application.debug ):
     from werkzeug.debug import DebuggedApplication
     application.wsgi_app = DebuggedApplication( application.wsgi_app, True )
